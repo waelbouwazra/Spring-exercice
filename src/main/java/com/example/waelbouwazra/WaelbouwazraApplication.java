e7c288179973e85cb1f8fa1161626a85bebcb357
waelbouwazra <98458757+waelbouwazra@users.noreply.github.com>
2023-01-05 22:51:33 +0100

    Init

---
+++ src/main/java/com/example/waelbouwazra/WaelbouwazraApplication.java
@@ -0,0 +1,13 @@
+package com.example.waelbouwazra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaelbouwazraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WaelbouwazraApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/waelbouwazra/WaelbouwazraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.waelbouwazra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaelbouwazraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
